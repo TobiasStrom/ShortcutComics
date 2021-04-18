9fb5fadfab3d403e7c83b57c2cbdd68c2c7329e0
Tobias Strøm <Tobias2409@gmail.com>
2021-04-18 15:56:19 +0200

    You can now browse through comics

--- lib/provides/comics_provider.dart
+++ lib/provides/comics_provider.dart
@@ -3,17 +3,27 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shortcut_comics/widgets/comics_item.dart';
 import '../models/comics.dart';
 
 class ComicsProvider with ChangeNotifier{
+
+  Comics _selectedComics;
+
+  Comics get selectedComics => _selectedComics;
 
   //Method of returning comics from api
   Future<Comics> fetchComics(int num) async{
     Comics comics;
     String url ='https://xkcd.com/'+num.toString()+'/info.0.json'; // url for searching
+    if(num == -1){ // if num is -1 get last comics
+      url = 'https://xkcd.com/info.0.json'; // url for last comics
+    }
     final response = await http.get(Uri.parse(url)); //make the http get request
     if(response.statusCode == HttpStatus.ok){//check if status code is ok;
       comics = Comics.fromJson(jsonDecode(response.body)); // convert response to Comics object
+      _selectedComics = comics;
+      notifyListeners();
       return comics;
     }else{ //if something did't go as planed return a default error comics
       comics = new Comics(
@@ -25,7 +35,23 @@ class ComicsProvider with ChangeNotifier{
         news :'',
         transcript :'',
       );
+      _selectedComics = comics;
+      notifyListeners();
       return comics;
     }
   }
+
+  Future<Comics> nextComics(){
+    print('Next: ${_selectedComics.num}');
+    return fetchComics(_selectedComics.num + 1);
+  }
+
+  Future<Comics> previousComics(){
+    print('Prev: ${_selectedComics.num}');
+    return fetchComics(_selectedComics.num - 1);
+  }
+
+  //Make it easy to remember what values means
+  int _first = -2;
+  int _last = -3;
 }
--- lib/screens/comics_screen.dart
+++ lib/screens/comics_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shortcut_comics/models/comics.dart';
 import 'package:shortcut_comics/provides/comics_provider.dart';
 import 'package:shortcut_comics/widgets/comics_item.dart';
+import 'package:shortcut_comics/widgets/rounded_button.dart';
 
 class ComicsScreen extends StatefulWidget {
   static final routeName = '/comics';
@@ -11,21 +12,21 @@ class ComicsScreen extends StatefulWidget {
 }
 
 class _ComicsScreenState extends State<ComicsScreen> {
-  Future<Comics> futureComics;
+  Future<Comics> _futureComics;
 
   @override
   void initState() {
     super.initState();
-
     final comicsData = context.read<ComicsProvider>();
-    futureComics = comicsData.fetchComics(2443);
-
+    _futureComics = comicsData.fetchComics(23);
+    print('Init');
   }
 
   @override
   Widget build(BuildContext context) {
 
     final mediaQuery = MediaQuery.of(context);// get media size
+    var comicsData = Provider.of<ComicsProvider>(context);
 
     return SafeArea(
       child: Scaffold(
@@ -33,10 +34,46 @@ class _ComicsScreenState extends State<ComicsScreen> {
           child: Container(
             width: mediaQuery.size.width, //Make screen full size
             child: FutureBuilder<Comics>(
-              future: futureComics,
+              future: _futureComics,
               builder: (context, snapshot) {
                 if(snapshot.hasData){
-                  return ComicsItem(snapshot.data);
+                  return Column(children: [
+                    Container(
+                      height: (mediaQuery.size.height - mediaQuery.padding.top)* 0.9,
+                      child: SingleChildScrollView(child: ComicsItem(snapshot.data)),
+                    ),
+                    Container(
+                      height: (mediaQuery.size.height - mediaQuery.padding.top)* 0.1,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            RoundedButton(
+                              text: 'Previous',
+                              width: mediaQuery.size.width * 0.3,
+                              height: 35,
+                              color: Colors.red,
+                              onClick: (){
+                                _futureComics = comicsData.previousComics();
+                              },
+                            ),
+                            RoundedButton(
+                              text: 'Next',
+                              width: mediaQuery.size.width * 0.3,
+                              height: 35,
+                              color: Colors.green,
+                              onClick: (){
+                                //comicsData.selectedComicId > comicsData.lastId ? null :
+                                _futureComics = comicsData.nextComics();
+                              },
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ),
+                    
+                  ],);
                 }
                 return Container(
                   height: (mediaQuery.size.height - mediaQuery.padding.top),
